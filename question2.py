c4f505734a61b8fc1c2e19272950bfb5f0cb746a
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-01-22 19:31:33 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -3,14 +3,17 @@ def question2_1():
     return answer
 
 def question2_2():
-
+    answer = ['Continuous', 'Ratio', 'Continuous because speed can take any value within a range. Ratio because It has a true zero (zero speed) and is measured numerically.']
     return answer
 
 def question2_3():
+    answer = ['Continuous', 'Ratio', 'Continuous because decibel levels can vary continuously. Ratio because decibel is a logarithmic scale but has a reference point that can be considered a true zero in practical terms.']
     return answer 
 
 def question2_4():
+    answer = ['Discrete', 'Ordinal', 'Discrete because the scale has distinct categories (1 to 5). Ordinal because the scale has an inherent order.']
     return answer
 
 def question2_5():
+    answer = ['Discrete', 'Nomial', 'Discrete because a finite set of distinct numbers. Nomial because it's used for identification, not for quantitative measurement.']
     return answer
