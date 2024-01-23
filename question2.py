37bb438e35bc9dfc67cb73feb4fd6bf3d0dcf44e
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-01-22 19:15:39 -0500

    Create question2.py

---
+++ question2.py
@@ -0,0 +1,16 @@
+def question2_1():
+    answer = ['Discrete', 'Ratio', 'It is discrete because the number of courses is countable and finite. It is Ratio because It's a count with a true zero (zero courses).']
+    return answer
+
+def question2_2():
+
+    return answer
+
+def question2_3():
+    return answer 
+
+def question2_4():
+    return answer
+
+def question2_5():
+    return answer
