c5335525bf905ef2ea5f760501f61f285f1ffe09
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-01-24 18:15:40 -0500

    Create question6.py

---
+++ question6.py
@@ -0,0 +1,21 @@
+def question6_1():
+    answer = {
+        'equal_width': ['Change', 3],
+        'equal_frequency': ['No change', 9]
+    }
+
+    return answer
+
+def question6_2():
+    answer = {
+        'equal_width': ['Change', 2],
+        'equal_frequency': ['No change', 9]
+    }
+    return answer
+
+def question6_3():
+    answer = {
+        'equal_width': ['Change', 2],
+        'equal_frequency': ['Change', 2]
+    }
+    return answer 
