61c4109804dc29e4620db860a0c6d7eccd7c7351
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-01-28 17:08:16 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -3,9 +3,9 @@ def question7_1():
     return answer
 
 def question7_2():
-    answer = 'non-increasing'
+    answer = 'increase/decrease'
     return answer
 
 def question7_3():
-    answer = [(0.0, 3), (3, 4), (4, 5), (5, 'infinity')]
+    answer = [(0, 3), (3, 5), (5, 7), (7, 'infinity')]
     return answer 
