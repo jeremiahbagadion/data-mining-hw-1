3e65e0c03df623f159484f6017f0c38ff8380864
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-01-28 15:54:39 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,19 +1,19 @@
 def question2_1():
-    answer = ['Discrete', 'Ratio', 'It is discrete because the number of courses is countable and finite. It is Ratio because Its a count with a true zero (zero courses).']
+    answer = ['discrete', 'quantitative', 'ratio']
     return answer
 
 def question2_2():
-    answer = ['Continuous', 'Ratio', 'Continuous because speed can take any value within a range. Ratio because It has a true zero (zero speed) and is measured numerically.']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question2_3():
-    answer = ['Continuous', 'Ratio', 'Continuous because decibel levels can vary continuously. Ratio because decibel is a logarithmic scale but has a reference point that can be considered a true zero in practical terms.']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer 
 
 def question2_4():
-    answer = ['Discrete', 'Ordinal', 'Discrete because the scale has distinct categories (1 to 5). Ordinal because the scale has an inherent order.']
+    answer = ['discrete', 'qualitative', 'ordinal']
     return answer
 
 def question2_5():
-    answer = ['Discrete', 'Nominal', 'Discrete because a finite set of distinct numbers. Nominal because its used for identification, not for quantitative measurement.']
+    answer = ['discrete', 'qualitative', 'nominal']
     return answer
