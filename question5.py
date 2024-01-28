cced6fa9de89ff4c5b2ffcb822fc4194c3de350d
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-01-28 16:08:33 -0500

    Update question5.py

--- question5.py
+++ question5.py
@@ -17,6 +17,7 @@ def question5_2():
 def question5_3():
     answer = {
             'bin1': [1,5,6,7],
-            'bin2': [2,3,4,8,9]
+            'bin2': [2,3,4,8,9],
+            'bin3': []
     }
     return answer 
